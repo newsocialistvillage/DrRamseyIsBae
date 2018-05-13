cd62742535e19dbfbb7ba9cadc1f2ecca41fae4b
zhengqi Yang <35822645+newsocialistvillage@users.noreply.github.com>
2018-05-13 04:01:14 -0400

    Add files via upload
    Final Project, 
    a twitterbot that works like a binary tree. 
    Provides two scenarios for an audience for decide by either liking or retweeting

---
+++ credentials.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 18:11:53 2018
+
+@author: charles
+"""
+# secret credentials
+
+
+consumer_key = '0LI3kjy4EklUWBuuN2bSvyJTd'
+consumer_secret = 'hlwe9gOYbB7vDbCe1h1Rtz0w1H0aTcZ2cnOCeE42N9ZkrfJdV0'
+access_token = '983820175320539136-MbrEIYULPwOo09YULmuZRWPBeBkKz1k'
+access_secret = 'd1keoC6CUhjOb7opWoRud8FiiKWUbdC6CySQl40YvUngy'
